daa729edd3cafbcf2c115cdfb426fc5cc7491c6d
Julie Kallini <jkallini@princeton.edu>
2020-04-25 20:09:20 -0700

    Create nltk downloader script

---
+++ nltk_download.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# nltk_download.py
+
+# BEFORE running this script, run 'pip install --user -U nltk' and
+# 'pip install --user -U numpy' in your terminal. Then, when you
+# run this script, a GUI will appear. Click 'download' to download
+# the nltk library files in your home directory.
+
+import nltk
+import ssl
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+nltk.download()
