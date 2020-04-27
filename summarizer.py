01280115dc6e98a9696dcb142b4e92449b8a66c1
Julie Kallini <jkallini@princeton.edu>
2020-04-26 20:25:54 -0700

    Complete Noun-Verb Chunking

--- summarizer.py
+++ summarizer.py
@@ -3,7 +3,9 @@
 
 import nltk
 from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.tokenize import StringTokenizer
 from nltk.corpus import wordnet
+from nltk import RegexpParser
 
 """
 Prepocess raw text data in file_name by segmenting the text into
@@ -11,6 +13,8 @@ sentences, and tokenizing all of the words. Uses POS-tagging to
 return a list of lists of tuples, where the tuples contain a
 word and its POS tag.
 """
+
+
 def preprocess(file_name):
 
     f = open(file_name, 'r')
@@ -36,8 +40,10 @@ def preprocess(file_name):
 Return the equivalent wordnet POS tag for the given nltk
 POS tag.
 """
+
+
 def get_wordnet_tag(nltk_tag):
-    
+
     if nltk_tag.startswith('J'):
         return wordnet.ADJ
     elif nltk_tag.startswith('V'):
@@ -56,13 +62,15 @@ Produce lemmas of all the words in each of the sentences.
 Returns a list of lists of tuples, where the first element is the
 lemma of the original word, and the second element is the POS tag.
 """
+
+
 def lemmatize(sentences):
 
     lemmatizer = WordNetLemmatizer()
     lemmatized = []
     for sent in sentences:
         new_sent = []
-        for word,tag in sent:
+        for word, tag in sent:
             wordnet_tag = get_wordnet_tag(tag)
             new_sent.append((lemmatizer.lemmatize(word, wordnet_tag), tag))
         lemmatized.append(new_sent)
@@ -74,13 +82,15 @@ Produce stems of all the words in each of the sentences.
 Returns a list of lists of tuples, where the first element is the
 stem of the original word, and the second element is the POS tag.
 """
+
+
 def stem(sentences):
-    
+
     stemmer = PorterStemmer()
     stemmed = []
     for sent in sentences:
         new_sent = []
-        for word,tag in sent:
+        for word, tag in sent:
             new_sent.append((stemmer.stem(word), tag))
         stemmed.append(new_sent)
     return stemmed
@@ -89,20 +99,53 @@ def stem(sentences):
 """
 Print side-by-side comparsion of lemmatization and stemming.
 """
+
+
 def lemma_vs_stem(sentences, lemmatized_sentences, stemmed_sentences):
     for i in range(len(lemmatized_sentences)):
         for j in range(len(lemmatized_sentences[i])):
-            print(sentences[i][j][0], lemmatized_sentences[i][j][0], \
-                stemmed_sentences[i][j][0])
+            print(sentences[i][j][0], lemmatized_sentences[i][j][0],
+                  stemmed_sentences[i][j][0])
 
+
+def chunk(sentences):
+    grammar = r"""
+        NN:     {<DT|PP\$>?<JJ>*<NN>}   # chunk determiner/possessive, adjectives and noun
+                {<DT|PP\$>?<JJ>*<NNS>}
+        NNP:    {<NNP>+}                # chunk sequences of proper nouns
+                {<NNP>*<NNPS>}
+        VP:     {<VB>}                  # chunk any kind of verb
+                {<VBD>}
+                {<VBN>}
+                {<VBG>}
+                {<VBP>}
+                {<VBZ>}
+"""
+    chunk_parser = RegexpParser(grammar)
+    chunked = []
+    for sent in sentences:
+        new_sent = []
+        tree = chunk_parser.parse(sent)
+        for subtree in tree.subtrees(lambda t: t.height() == 2):
+            new_word = ' '.join([ w for (w, t) in subtree.leaves() ])
+            new_tag = subtree.label()
+            new_sent.append((new_word, new_tag))
+        chunked.append(new_sent)
+    return chunked
 
 """
 Main function.
 """
+
+
 def main():
     sentences = preprocess('covid.txt')
     lemmatized_sentences = lemmatize(sentences)
     stemmed_sentences = stem(lemmatized_sentences)
+    chunked_sentences = chunk(stemmed_sentences)
+    for sent in sentences:
+        print(sent)
+
 
 if __name__ == '__main__':
     main()
