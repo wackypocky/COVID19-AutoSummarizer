3ff5b157c2ff0ab919ee3281a1aaf75b698d2c0b
Julie Kallini <jkallini@princeton.edu>
2020-05-08 18:02:00 -0700

    Fix encoding error

--- noninteractive_summarizer.py
+++ noninteractive_summarizer.py
@@ -13,6 +13,21 @@ TITLE = ""
 DATE = ""
 REGION = ""
 
+_surrogates = re.compile(r"[\uDC80-\uDCFF]")
+
+def detect_decoding_errors_line(l, _s=_surrogates.finditer):
+    """Return decoding errors in a line of text
+
+    Works with text lines decoded with the surrogateescape
+    error handler.
+
+    Returns a list of (pos, byte) tuples
+
+    """
+    # DC80 - DCFF encode bad bytes 80-FF
+    return [(m.start(), bytes([ord(m.group()) - 0xDC00]))
+            for m in _s(l)]
+
 """
 Prepocess raw text data in file_name by segmenting the text into
 sentences, and tokenizing all of the words. Uses POS-tagging to
@@ -26,7 +41,7 @@ def preprocess(file_name, num_sentences):
     global REGION
 
     try:
-        with open(file_name, 'r') as f:
+        with open(file_name, 'r', encoding="utf8", errors="surrogateescape") as f:
             TITLE = f.readline()  # title of article
             DATE = f.readline()
             REGION = f.readline()
